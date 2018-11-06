0f15366ab2fdd37401f805124bdd8238c86f072a
nyamu01b <brian.nyamu@flightdataservices.com>
2018-11-06 17:12:51 +0000

    Added article list and article detail

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Article
 
@@ -8,3 +9,9 @@ def article_list(request):
                   'articles/article_list.html',
                   {'articles': articles}
                   )
+
+
+def article_detail(request, slug):
+    article = Article.objects.get(slug=slug)
+    return render(request, 'articles/article_detail.html', { 'article': article })
+
