10ef8eb2e847ea6c09851cdbaa8df966b0bf0ee3
nyamu01b <brian.nyamu@flightdataservices.com>
2018-12-29 21:52:15 +0000

    Allowed hosts

--- name/settings.py
+++ name/settings.py
@@ -90,9 +90,14 @@ USE_MODELTRANSLATION = False
 # MAIN DJANGO SETTINGS #
 ########################
 
+SECRET_KEY = os.environ['SECRET_KEY']
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+         '0.0.0.0',
+         'localhost',
+        'youdea.herokuapp.com'
+         ]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
