c9fb20bf92b3994b3aa605b41927f77d14460d73
bryoh <brianbryo@gmail.com>
2019-06-07 23:12:34 +0100

    Changed media root

--- app/app/settings/base.py
+++ app/app/settings/base.py
@@ -171,7 +171,7 @@ STATICFILES_FINDERS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
 
 
