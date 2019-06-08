e50f22bbda7a296113bc103502a3d109685e93e7
bryoh <brianbryo@gmail.com>
2019-06-08 10:29:42 +0100

    small check

--- app/app/settings/prod.py
+++ app/app/settings/prod.py
@@ -34,7 +34,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '/static/'),
 ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # To send email from the server, we recommend django_sendmail_backend
