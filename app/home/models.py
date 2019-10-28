22ed28dde5acc16f6ab14d9afbf57d6b673b70bb
bryoh <brianbryo@gmail.com>
2019-10-28 17:52:15 +0000

    streamfield improvements

--- app/home/models.py
+++ app/home/models.py
@@ -16,7 +16,7 @@ class HomePage(Page):
     banner_title = models.CharField(max_length=100, blank=False, null=True)
     banner_subtitle = RichTextField(features=["bold", "italic"])
     banner_author = RichTextField(features=["italic"])
-    banner_image = models.ForeignKey("wagtailimages.Image", null=True, blank=False, on_delete=models.SET_NULL, related_name="+")
+    banner_image = models.ForeignKey("wagtailimages.Image", null=True, blank=True, on_delete=models.SET_NULL, related_name="+")
     banner_cta = models.ForeignKey("wagtailcore.Page", null=True, blank=True, on_delete=models.SET_NULL, related_name="+")
     featured_podcast = models.ForeignKey('wagtailmedia.Media', null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
 
