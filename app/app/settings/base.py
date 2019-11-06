c043670851b63b84baa70a0e96d92e1f163c071e
bryoh <brianbryo@gmail.com>
2019-11-06 14:09:16 +0000

    Social site settings

--- app/app/settings/base.py
+++ app/app/settings/base.py
@@ -30,7 +30,9 @@ INSTALLED_APPS = [
     'search',
     'flex',
     'streams',
+    'site_settings',
     'wagtail.contrib.forms',
+    'wagtail.contrib.settings',
     'wagtail.contrib.redirects',
     'wagtail.embeds',
     'wagtail.sites',
@@ -78,6 +80,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'wagtail.contrib.settings.context_processors.settings',
             ]
         },
     }
