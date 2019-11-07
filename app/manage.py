f8780ed1a439a410ab01c901487afd544b84e246
bryoh <brianbryo@gmail.com>
2019-11-07 15:32:20 +0000

    simple if statement debug/production

--- app/manage.py
+++ app/manage.py
@@ -3,7 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings.dev")
+    if os.environ.get('DEBUG', 'False') == 'True':
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings.dev")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings.production")
 
     from django.core.management import execute_from_command_line
 
