1d26360c96946867adec163ad4804669f518100f
bryoh <brianbryo@gmail.com>
2019-11-19 16:44:34 +0000

    codered initial commit

--- app/app/settings/dev.py
+++ app/app/settings/dev.py
@@ -3,22 +3,19 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# SECURITY WARNING: define the correct hosts in production!
+
 ALLOWED_HOSTS = ['*']
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-STATIC_ROOT = 'static'  # os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = 'media'  # os.path.join(BASE_DIR, 'media')
 
-INSTALLED_APPS = INSTALLED_APPS + ['debug_toolbar']
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+INSTALLED_APPS += ['django_sass', 'debug_toolbar']
 
 MIDDLEWARE = MIDDLEWARE + ['debug_toolbar.middleware.DebugToolbarMiddleware']
 INTERNAL_IPS = ("127.0.0.1", "172.17.0.1", "staging.herokuapp.com")
 
+WAGTAIL_CACHE = False
+
 try:
-    from .local import *
+    from .local_settings import *
 except ImportError:
     pass
--- app/app/urls.py
+++ app/app/urls.py
@@ -1,34 +1,32 @@
 from django.conf import settings
-from django.conf.urls import include, url
-from django.conf.urls.static import static
+from django.urls import include, path, re_path
 from django.contrib import admin
-
-from wagtail.admin import urls as wagtailadmin_urls
-from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
-
-from search import views as search_views
+from coderedcms import admin_urls as coderedadmin_urls
+from coderedcms import search_urls as coderedsearch_urls
+from coderedcms import urls as codered_urls
 
 urlpatterns = [
-    url(r'^django-admin/', admin.site.urls),
-    url(r'^admin/', include(wagtailadmin_urls)),
-    url(r'^documents/', include(wagtaildocs_urls)),
-    url(r'^search/$', search_views.search, name='search'),
+    # Admin
+    path('django-admin/', admin.site.urls),
+    path('admin/', include(coderedadmin_urls)),
+
+    # Documents
+    path('docs/', include(wagtaildocs_urls)),
+
+    # Search
+    path('search/', include(coderedsearch_urls)),
+
     # For anything not caught by a more specific rule above, hand over to
-    # Wagtail's page serving mechanism. This should be the last pattern in
+    # the page serving mechanism. This should be the last pattern in
     # the list:
-    url(r'', include(wagtail_urls)),
-    # Alternatively, if you want Wagtail pages to be served from a subpath
+    re_path(r'', include(codered_urls)),
+
+    # Alternatively, if you want CMS pages to be served from a subpath
     # of your site, rather than the site root:
-    #    url(r'^pages/', include(wagtail_urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    #    re_path(r'^pages/', include(codered_urls)),
+]
 
-from django.urls import path
-
-if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns = [path('__debug__/', include(debug_toolbar.urls))] + urlpatterns
 
 if settings.DEBUG:
     from django.conf.urls.static import static
