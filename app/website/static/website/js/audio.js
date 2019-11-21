b40a3d22392dc9db61544da6a5882f7a788d5a09
bryoh <brianbryo@gmail.com>
2019-11-21 15:10:44 +0000

    shouldve included this in my last commit

---
+++ app/website/static/website/js/audio.js
@@ -0,0 +1,44 @@
+'use strict'
+var supportsAudio = !! document.createElement('audio').canPlayType;
+if (supportsAudio) {
+    var player = new Plyr('#audio', { 
+        controls: [
+            'rewind',
+            'play',
+            'progress',
+            'current-time',
+            'duration',
+            'fast-forward',
+            'download',
+        ]
+    });
+    var currentTrack = 0,
+        playing = false,
+        npAction = $('#npAction'),
+        npTitle = $('#npTitle'),
+        //audio = $('#audio').on('play', function() {
+            //playing = true;
+            //npAction.text('Playing...');
+        //}).on('pause', function() {
+            //playing = false;
+            //npAction.text('Paused...')
+        //}).on('ended', function() {
+            //playing = false;
+            //npAction.text('Paused...')
+        //}).get(0),
+        row = $('.clickable-row').click(function (){
+            var fileurl = $(this).attr('data-href');
+            audio.src = fileurl; 
+            //alert(fileurl);
+            player.on('loadedmetadata', function() {
+                $('a[data-plyr="download"]').attr('href', fileurl);
+            });
+            audio.play();
+        })
+
+
+} else {
+    // no audio suppport
+    var noSupport = 'No support'
+}
+
